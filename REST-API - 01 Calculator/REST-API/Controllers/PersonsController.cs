bedc8b94bb7af8e25c43248aeac4d0a7cc96eba4
joao vitor <joaovitormrn@gmail.com>
2019-07-18 11:26:30 -0300

    Implementing all principal verbs

--- REST-API - 01 Calculator/REST-API/Controllers/PersonsController.cs
+++ REST-API - 01 Calculator/REST-API/Controllers/PersonsController.cs
@@ -8,35 +8,35 @@ namespace REST_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class PersonsController : ControllerBase
     {
-        // GET api/values
+        // GET api/Persons
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/Persons/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/Persons
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/Persons/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/Persons/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
---
+++ REST-API - 01 Calculator/REST-API/Model/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTAPI.Model
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FristName { get; set; }
+        public string LastName { get; set; }
+        public string Adderss { get; set; }
+        public string Gender { get; set; }
+    }
+}

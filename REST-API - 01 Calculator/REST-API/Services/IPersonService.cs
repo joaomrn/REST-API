1e62fe3ba9c5b2ea62e1c5e38df5cf8a0e0a0ef6
Joao Vitor <joaovitormrn@gmail.com>
2019-12-26 21:52:25 -0300

    Development IPersonService and code model.

---
+++ REST-API - 01 Calculator/REST-API/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RESTAPI.Model;
+using System.Collections.Generic;
+
+namespace RESTAPI.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
--- REST-API - 01 Calculator/REST-API/Startup.cs
+++ REST-API - 01 Calculator/REST-API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RESTAPI.Services;
+using RESTAPI.Services.Implementattions;
 
 namespace REST_API
 {
@@ -26,6 +28,9 @@ namespace REST_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImpl>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

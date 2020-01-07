a47097622135b8480e1e4aaf7c525a2fb2fd2d5b
Joao Vitor <joaovitormrn@gmail.com>
2020-01-06 22:16:31 -0300

    Adicionado autenticação JWT

---
+++ REST-API - 01 Calculator/REST-API/Business/Implementattions/UserBusinessImpl.cs
@@ -0,0 +1,18 @@
+﻿using RESTAPI.Model;
+using RESTAPI.Repository;
+
+namespace RESTAPI.Business.Implementattions
+{
+    public class UserBusinessImpl : IUserBusiness
+    {
+        private IUserRepository _userRepository;
+        public UserBusinessImpl(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public object FindByLogin(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ REST-API - 01 Calculator/REST-API/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RESTAPI.Business;
+using RESTAPI.Model;
+
+namespace RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        //Declaração do serviço usado
+        private IUserBusiness _userBusiness;
+
+        public LoginController(IUserBusiness userBusiness)
+        {
+            _userBusiness = userBusiness;
+        }
+
+        // POST api/books
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult Post([FromBody]User user)
+        {
+            if (user == null) return BadRequest();
+            return new ObjectResult(_userBusiness.FindByLogin(user));
+        }
+    }
+}
